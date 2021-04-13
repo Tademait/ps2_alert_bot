bb6f8375b204d8c58d4ae3a97bb0751741439064
Tademait <Tademait@github.com>
2021-04-13 22:07:02 +0200

    added the rest of the files

---
+++ main.py
@@ -0,0 +1,64 @@
+import discord
+import json, urllib, math, schedule, time, sys, os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+
+
+def getEventInfo():
+    with urllib.request.urlopen( # TODO add a timeout interval for the API server since it's a piece of $@!#
+            "https://census.daybreakgames.com/get/ps2:v2/world_event/?type=METAGAME&world_id=13&c:limit=1") as url:
+        data = json.loads(url.read().decode())
+        for p in data['world_event_list']:
+            event_id = int(p['metagame_event_id'])
+            timestamp = int(p['timestamp'])
+            event_state = p["metagame_event_state_name"]
+
+    with open("metagame_event.json", "r") as f:
+        eventy_txt = f.read()
+        eventy_json = json.loads(eventy_txt)
+
+    ##get time funkce
+    current_time = math.ceil(datetime.now().timestamp())
+    running_time = ((current_time - timestamp) / 60)
+    running_time = round(running_time, 2)
+
+    if (event_state == "ended"):
+        print("STARTED")
+    #    if ((158 >= event_id) and (event_id >= 123)) or ((193 >= event_id) and (
+                #event_id >= 183)):  ##filtruje pouze main alerty, bez if je možné psát info o všech probíhajících eventech
+        for h in eventy_json["metagame_event_list"]:
+            if (event_id == int(h["metagame_event_id"])):
+                event_info_name = h["name"]["en"]
+                event_info_desc = h["description"]["en"]
+                return event_info_name, event_info_desc
+    print("no event running")
+    return "N/A"    # in case function fails to return a tuple with the info
+
+def getTime():
+    return datetime.now().strftime("[%H:%M:%S]:")
+    
+@client.event
+async def on_ready():
+    print("{0} Logged in as {1.user}".format(getTime(), client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "!hi":
+        await message.channel.send("Hello, soldier.")
+    if message.content == "!help":
+        await message.channel.send(" !alert info [server name] - prints out the current status on given server.\n"
+                                   " !hi - show the bot some attention and love.\n")
+    if message.content == "!alert info":
+        info = getEventInfo()
+        if (info == "N/A"):
+            await message.channel.send("No info available")
+        else:
+            await message.channel.send(info[0] + "\n" + info[1])
+client.run(TOKEN)
