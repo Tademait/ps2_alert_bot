351fdd83273ef5faf63ebb1aac3f08ddd747ad39
Tademait <Tademait@github.com>
2021-05-26 12:00:00 +0200

    added new information and embed styling

--- main.py
+++ main.py
@@ -36,6 +36,11 @@ def getEventInfo():
         event_id = int(p['metagame_event_id'])
         timestamp = int(p['timestamp'])
         event_state = p["metagame_event_state_name"]
+        factionPercentage = []
+        factionPercentage.append("🟦 NC: " + p["faction_nc"][0:4] + "%")
+        factionPercentage.append("🟥 TR: " + p["faction_tr"][0:4] + "%")
+        factionPercentage.append("🟪 VS: " + p["faction_vs"][0:4] + "%")
+
 
     with open("metagame_event.json", "r") as f:
         eventy_txt = f.read()
@@ -45,16 +50,18 @@ def getEventInfo():
     current_time = math.ceil(datetime.now().timestamp())
     running_time = ((current_time - timestamp) / 60)
     running_time = round(running_time, 2)
+    running_time = str(running_time)
 
-    if (event_state == "ended"):    # change this back to started, used only for debugging purposes
-        print("STARTED")
-    # this if statement filters out only the "main" in-game meta alerts (the event IDs may need a revisit due to a  game update):
+    if (event_state == "ended"):
+        return "ENDED"
+    if (event_state == "started"):    # change this back to started, used only for debugging purposes
+    # this if statement filters out only the "main" in-game meta alerts (the event IDs may need a revisit due to a game update):
     #    if ((158 >= event_id) and (event_id >= 123)) or ((193 >= event_id) and (event_id >= 183)):
-        for h in eventy_json["metagame_event_list"]:
-            if (event_id == int(h["metagame_event_id"])):
-                event_info_name = h["name"]["en"]
-                event_info_desc = h["description"]["en"]
-                return event_info_name, event_info_desc
+        for entry in eventy_json["metagame_event_list"]:
+            if (event_id == int(entry["metagame_event_id"])):
+                event_info_name = entry["name"]["en"]
+                event_info_desc = entry["description"]["en"]
+                return event_info_name, event_info_desc, running_time, factionPercentage
     print("no event running")
     return "N/A"    # in case function fails to return a tuple with the info
 
@@ -67,24 +74,36 @@ def getTime():
 async def on_ready():
     print("{0} Logged in as {1.user}".format(getTime(), client))
 
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     # maybe define a complete premade embed instead of just color later
     orange = discord.colour.Color.from_rgb(236, 88, 9)
-    if message.content == "!hi":
+    if message.content == "?hi":
         hello_embed = discord.Embed(title="UwU", description="Hello {}".format(message.author), color=orange)
         hello_embed.set_image(url="https://cdn.betterttv.net/emote/60448132306b602acc598647/3x.gif")
         await message.channel.send(embed=hello_embed)
-    if message.content == "!help":
-        await message.channel.send(" !alert info [server name] - prints out the current status on given server.\n"
-                                   " !hi - show the bot some attention and love.\n")
-    if message.content == "!alert info":
+
+    if message.content == "?help":
+        help_embed = discord.Embed(title="Help", description="Usable bot commands: ", color=orange)
+        help_embed.add_field(name="?alert info [server name]", value="prints out the current status on given server.", inline=True)
+        help_embed.add_field(name="?hi", value="show the bot some attention and love.", inline=True)
+        await message.channel.send(embed=help_embed)
+
+    if message.content == "?alert info":
         info = getEventInfo()
         if (info == "N/A"):
             await message.channel.send("No info available")
+        if (info == "ENDED"):
+            alert_embed = discord.Embed(title="Currently running events:", description="There is no event running at the moment", color=orange)
+            await message.channel.send(embed=alert_embed)
         else:
-            await message.channel.send(info[0] + "\n" + info[1])
+            print(info[0] + "\n" + info[1] + "\n" + info[2])
+            # await message.channel.send(info[0] + "\n" + info[1] + "\n" + "Elapsed time: " + info[2] + " minutes")
+            alert_embed = discord.Embed(title="Currently running events:",description="\n", color=orange)
+            alert_embed.add_field(name=info[0], value=info[1] + "\n" + "Elapsed time: " + info[2] + " minutes", inline=True)
+            alert_embed.add_field(name="Current score", value=info[3][0] + " " + info[3][1] + " " + info[3][2], inline=True)
+            alert_embed.set_footer(text=message.author, icon_url="https://logo-logos.com/wp-content/uploads/2018/03/discordlogo.png")
+            await message.channel.send(embed=alert_embed)
 client.run(TOKEN)
